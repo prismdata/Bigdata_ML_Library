6915d1ca0f2ad43e7b315ab03fe95e17a59f075c
prismdata <prismdata@naver.com>
2019-05-23 17:21:52 +0900

    이전 코드 삭제/구조 변경1

---
+++ ankus4pass_DLib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ankus4pass_DLib',
+    version='0.0.1',
+    packages=['ankus4passDALib', 'ankus4passDALib.example', 'ankus4passDALib.unsupervised'],
+    url='',
+    license='apache-2.0',
+    author='prismdata',
+    author_email='',
+    description=''
+)
